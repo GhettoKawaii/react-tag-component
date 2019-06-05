b27efdad511ab958cc5dbdc0c6e54017b988247f
Alex DyaDya <neobro024@gmail.com>
2019-06-05 11:22:56 +0300

    Init commit

---
+++ src/TagComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class TagComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is React-Tag-Component</h1>
+        <input type="text" placeholder="Waiting for your tag..." />
+      </div>
+    );
+  }
+}
